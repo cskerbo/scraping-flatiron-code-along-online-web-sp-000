7f4ef938776978e232688b4fb93cccbffa8c3e3b
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-08 07:47:25 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -3,6 +3,12 @@ class Course
 
     attr_accessor :title, :schedule, :description
 
+    @@all = []
+
+    def initialize
+      @@all << self
+    end
+
     def self.all
       Course.all
     end
